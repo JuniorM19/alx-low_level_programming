278210e39156bdd21e8d0d56ede462b167f7161b
Junior <jm.mnisi@icloud.com>
2023-03-07 05:42:50 -0800

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	int i, sum = 0;
+	int i, s = 0;
 
-	for (i = 0; i < 1024; i++)
+	while (i < 1024)
 	{
-	
-	if ((i % 3) == 0 || (i % 5) == 0);
-	sum += i;
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			s += i;
+		}
+		i++
 	}
-
-	printf("%d\n", sum);
-
-	return (0);
+	printf("%d\n", s)
+		return (0);
 }
