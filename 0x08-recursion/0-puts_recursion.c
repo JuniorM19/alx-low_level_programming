2996ddc04bae25d3816862292f880bb12d2d0267
JuniorM19 <jm.mnisi@icloud.com>
2023-05-14 17:48:29 -0700

    Puts with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,19 +3,19 @@
 /**
  * main - a function that prints a string.
  * @s - source string.
- *
+ *_puts_recursion - prints a string.
  * Return: string to be printed.
  */
 
 void _puts_recursion(char *s)
 {
-        if (*s == 0)
+if (*s == 0)
 	{
-         putchar('\n');
+        putchar('\n');
 	}
 	else
 	{
 		putchar(*s);
-			_puts_recursion(s + 1);
+		_puts_recursion(s + 1);
 	}
 }
