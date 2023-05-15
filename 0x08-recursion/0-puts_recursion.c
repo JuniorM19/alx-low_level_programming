972a067a9942efce3ad5f5dd4606d4e26427638d
JuniorM19 <jm.mnisi@icloud.com>
2023-05-14 17:54:16 -0700

    Puts with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,12 +10,12 @@
 void _puts_recursion(char *s)
 {
 if (*s == 0)
-	{
-        putchar('\n');
-	}
-	else
-	{
-		putchar(*s);
-		_puts_recursion(s + 1);
-	}
+{
+putchar('\n');
+}
+else
+{
+putchar(*s);
+_puts_recursion(s + 1);
+}
 }
