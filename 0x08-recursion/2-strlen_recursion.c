443d63dc6efcfbdb2e6ccd76c319c7c874bb1374
JuniorM19 <jm.mnisi@icloud.com>
2023-05-22 04:24:08 -0700

    3-factorial.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - function that returns the length of a string.
+ *
+ * @s: source string
+ * _strlen_recursion: prints the string length.
+ *
+ * Return: the string length.
+ */
+
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + (_strlen_recursion(s + 1));
+	}
+}
---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - fuction that returns a factorial.
+ * @n: numer that returns a factorial.
+ * Return: factorial of n.
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	
+		return (-1);
+	
+	if (n == 0)
+	
+		return (1);
+	return (n * (n - 1));
+}
